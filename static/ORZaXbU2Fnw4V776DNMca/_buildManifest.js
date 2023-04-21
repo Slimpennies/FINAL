11fc24b4da75b634cadcfab7fe3d84ae37dbcc21
Slimpennies <128172825+Slimpennies@users.noreply.github.com>
2023-04-21 15:28:26 -0400

    Add files via upload

---
+++ static/ORZaXbU2Fnw4V776DNMca/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3f122c0bce4f3e37.css","static/chunks/pages/index-b65aab8a9ef27c6c.js"],"/_error":["static/chunks/pages/_error-82b79221b9ed784b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
